31e85b646b231e9b6f18494a268dd2f00a937851
santosh <santosh@pathfinder.global>
2019-07-20 18:46:42 +0530

    Added requests to install requires

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ author = 'Santosh Elangovan aka Catalyst18',
 author_email = 'etsant.18@gmail.com',
 packages = ['nse_analyzer'],
 python_requires='!=2.7, !=3.0.*, !=3.1.*, !=3.2.*, >=3.3.*, <4',
-install_requires = ['psycopg2','pandas','configparser',],
+install_requires = ['psycopg2','pandas','configparser','requests'],
 project_urls={
         'Bug Reports': 'https://github.com/Catalyst18/nationalstock_analyzer/issues',
         'Say Thanks!': 'https://www.linkedin.com/in/santosh-elangovan-63721b47/',
